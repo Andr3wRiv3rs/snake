4c312c2639d6c89d4b45c2747c192f77f05b2b48
Andr3wRiv3rs <andrewarivers@gmail.com>
2018-08-02 19:10:42 -0700

    Separated snake logic.

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -1,10 +1,6 @@
 let socket = io();
 let $ = e => (document.querySelector(e));
 
-let layers = [$('#canvas-view'), document.createElement('canvas')];
-
-let camera = layers[0];
-
 let global = {
     fps: 20
 };
@@ -12,6 +8,11 @@ let global = {
 let viewport = {
     width: 32
 };
+
+// Initialize layers.
+
+let layers = [$('#canvas-view'), document.createElement('canvas')];
+let camera = layers[0];
 
 for (let i in layers) {
     let l = layers[i];
@@ -36,18 +37,27 @@ for (let i in layers) {
     };
 }
 
+// Initialize step events.
+
 let step = {
     begin: [],
     mid: [],
     end: []
 };
 
+// Game loop.
+
 let gameLoop = setInterval(()=>{
-    let keys = Object.keys(step);
+
+    // Adjust camera dimensions.
+
     camera.width = viewport.width; 
     camera.height = viewport.width / (camera.clientWidth/camera.clientHeight);
 
-    updateGamepads();
+    updateGamepads(); // Polls gamepads and formats input.
+
+    // Execute step events.
+    
     for (let i in step.begin) {
         step.begin[i]();
     }
@@ -59,74 +69,65 @@ let gameLoop = setInterval(()=>{
     for (let i in step.end) {
         step.end[i]();
     }
-    clearPress();
+
+    clearPress(); // Clears press events for the current frame.
+
 }, 1000/global.fps);
 
+// Snake object, handles drawing and pushing events to gameLoop.
+
 let Snake = function ( options ) { 
+
     options = options || {};
-
-    this.layer = options.layer || 0;
-    this.x = options.x || 12;
-    this.y = options.y || 12;
-
-    this.color = 'white';
-
-    this.directions = {
-        up: [0,-1],
-        down: [0,1],
-        left: [-1,0],
-        right: [1,0]
-    } 
-
-    this.direction = 0;
-
-    let dw = ['up','down','left','right'];
-
-    this.units = [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2];
 
     if ( (typeof options != 'object' ) || Array.isArray(options) ) {
         throw '[Snake] options paramater must be an object.'
     }
 
+    this.color = 'white';
+    this.direction = 0;
+    this.layer = options.layer || 0;
+    this.x = options.x || 12;
+    this.y = options.y || 12;
+
+    this.cors = [];
+
+    this.units = [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2];
+
     this.step = () => {
-        this.clear()
-        try{options.step()}catch(e){};
+        this.clear(this.cors);
 
-        this.direction = input.direction;
+        try{options.step()}catch(e){}; // Needs changing, used to ignore "undefined" errors.
+        let data = updateSnake(this.x,this.y,this.direction,this.units, [/* Put collision coordinates here. */]);
 
-        let d = this.directions[dw[this.units[0]]];
-        let m = this.directions[dw[this.direction]];
-        this.x+=m[0];
-        this.y+=m[1];
-        this.units.splice(this.units.length-1,1);
-        this.units.reverse();
-        this.units.push(this.direction);
-        this.units.reverse();
-        this.draw();
+        this.x=data.x;
+        this.y=data.y;
+
+        this.draw(data.cors);
+        this.cors = data.cors
+
     };
 
-    this.draw = () => {
-        let pos = [this.x,this.y];
-        for (let i in this.units) {
-            let m = this.directions[dw[this.units[i]]];
-            pos[0] -= m[0];
-            pos[1] -= m[1];
-            layers[this.layer].draw.point(pos[0],pos[1],this.color);
+    this.draw = (cors) => {
+        for (let i in cors) {
+            layers[this.layer].draw.point(cors[i][0],cors[i][1],this.color);
         };
     };
 
-    this.clear = () => {
-        let pos = [this.x,this.y];
-        for (let i in this.units) {
-            let m = this.directions[dw[this.units[i]]];
-            layers[this.layer].clear.point(this.x-m[0],this.y-m[1],this.color);
+    this.clear = (cors) => {
+        for (let i in cors) {
+            layers[this.layer].clear.point(cors[i][0],cors[i][1]);
         };
     };
 
     this.create = () => {
         try{options.create()}catch(e){};
-        step.mid.push(this.step);
+        step.mid.push(this.step); // Pushes step event to gameLoop.
     };
 
     this.create();
 }
+
+let player = new Snake({step: () => {
+    player.direction = input.direction;
+}});
---
+++ client/scripts/snake.js
@@ -0,0 +1,60 @@
+let updateSnake = (x,y,direction,units,cols) => {
+    let directions = {
+        up: [0,-1],
+        down: [0,1],
+        left: [-1,0],
+        right: [1,0]
+    }
+
+    let collision = [false, false]; // [is colliding, head colliding]
+
+    let dw = ['up','down','left','right'];
+    
+    // Update units
+    if(cols){
+        units.splice(units.length-1,1);
+        units.reverse();
+        units.push(direction);
+        units.reverse();
+    }
+
+    // Find coordinates
+
+
+    let m = directions[dw[direction]];
+
+    let cors = [];
+    let pos = [x+m[0],y+m[1]];
+
+    for (let i in units) {
+        let d = directions[dw[units[i]]];
+
+        cors.push([pos[0],pos[1]]);
+
+        pos[0] = pos[0] - d[0];
+        pos[1] = pos[1] - d[1];
+    };
+
+    // Test collision
+
+    if(cols){
+        for(let i in cors){
+            for(let z in cols){
+                if(cors[i][0]==cols[z][0] && cors[i][1]==cols[z][1]){
+                    collision = [true,false];
+                    if(i==0){
+                        collision = [true,true];
+                    }
+                }
+            }
+        }
+    }
+
+    return {
+        x: x+m[0],
+        y: y+m[1],
+        units: units,
+        cors: cors,
+        collision: collision
+    }
+};
